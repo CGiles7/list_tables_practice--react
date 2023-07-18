9a9db5a13cc055490b05ba2da7a8fd94aee9fb1a
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-18 19:12:02 -0400

    add table and conditional rendering

--- src/Roster.js
+++ src/Roster.js
@@ -1,7 +1,43 @@
 import React from "react";
 
 function Roster({ detailed, roster }) {
-  return <p>Replace this with your solution.</p>
+  let table;
+  
+  switch(true) {
+    case detailed === false:
+      table = (
+      <ol>
+        {roster.map(({firstName}) => 
+        <li>{firstName}</li>
+        )}
+      </ol>
+      )
+      break;
+    default: {
+      table = (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+          <tbody>
+        {roster.map(({id, firstName, lastName, location}) => <tr key={firstName}>
+            <td>{id}</td>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>{location}</td>
+            </tr>)}
+          </tbody>
+        </table>
+      )
+    }
+      break;
+  }
+  return table;
 }
 
 export default Roster;
